89fee590bb93b3d00b442bfc8bf414edea93906f
Valentyn Karpenko <vallentine.karpenko@gmail.com>
2020-04-08 21:39:32 +0300

    banking microservice

--- MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -1,7 +1,10 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infrastructure.Bus;
@@ -18,7 +21,11 @@ namespace MicroRabbit.Infrastructure.IoC
         {
             // EventBus
 
-            //services.AddTransient<IEventBus, RabbitMQBus>();
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            // Domain Banking Commands
+
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
             //Banking
 
